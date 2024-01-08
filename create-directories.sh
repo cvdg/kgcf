a6e0b2d9feff8605dc138ee61baf61bb5eb1217c
Cees van de Griend <cees@srv00.griend.dev>
2024-01-08 10:57:36 +0100

    Added: radarr, lidarr and readarr

--- create-directories.sh
+++ create-directories.sh
@@ -3,11 +3,11 @@
 set -ex
 
 # App data on SSD
-sudo mkdir -pv /appdata/{gluetun,jellyfin,lidarr,prowlarr,qbittorrent,radarr,readarr,sonarr,transmission}
+sudo mkdir -pv /appdata/{gluetun,jellyfin,lidarr,prowlarr,radarr,readarr,sonarr,transmission}
 
 # Media data on HD
 sudo mkdir -pv /data/{media,torrent}/{audio,book,movie,tv}
 
-sudo chown -Rv cees:cees /appdata /data
 
+sudo chown -Rv cees:cees /appdata /data
 podman unshare chown -Rv cees:cees /appdata /data
